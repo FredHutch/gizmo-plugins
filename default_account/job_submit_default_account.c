fc64527480f7efb83f8034b2b3d0b064e742c869
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-08-16 12:49:43 -0700

    Fixed formatting and indenting

--- default_account/job_submit_default_account.c
+++ default_account/job_submit_default_account.c
@@ -77,11 +77,10 @@ static char *_get_default_account(uint32_t user_id)
 
 	memset(&user, 0, sizeof(slurmdb_user_rec_t));
 	user.uid = user_id;
-	if (assoc_mgr_fill_in_user(acct_db_conn,
-				   &user, 0, NULL) != SLURM_ERROR) {
-		return user.default_acct;
-	} else {
-		return NULL;
+	if (assoc_mgr_fill_in_user(acct_db_conn, &user, 0, NULL) != SLURM_ERROR) {
+    return user.default_acct;
+  } else {
+    return NULL;
 	}
 }
 
@@ -97,65 +96,66 @@ extern int job_submit(struct job_descriptor *job_desc, uint32_t submit_uid)
 	ListIterator part_iterator;
 	struct part_record *part_ptr;
 	struct part_record *top_prio_part = NULL;
-    char *account = NULL;
-    char *part_req = NULL;
+  char *account = NULL;
+  char *part_req = NULL;
 
-    debug("default_account: starting plugin") ;
+  debug("default_account: starting plugin") ;
 
 	if (job_desc->partition) {	/* job already specified partition */
-        debug( "default_account: partition %s set by command line", job_desc->partition );
-		return SLURM_SUCCESS;
+    debug(
+        "default_account: partition %s set by command line",
+        job_desc->partition
+        );
+    return SLURM_SUCCESS;
+  }
+
+  /* check job's account */
+  debug( "default_account: checking job account" );
+  if (job_desc->account){
+    debug("default_account: account %s requested", job_desc->account);
+    account = job_desc->account ;
+  } else {
+    /* else check user's default account */
+    account = _get_default_account( job_desc->user_id );
+    debug( "default_account: got users default account %s", account );
+  }
+
+  /* look for partition matching account name */
+  part_iterator = list_iterator_create(part_list);
+  while ((part_ptr = (struct part_record *) list_next(part_iterator))) {
+    debug( "default_account: checking partition %s against %s",
+        part_ptr->name, account );
+    if (strcmp(part_ptr->name,account) == 0){
+      job_desc->partition = xstrdup(account);
+      info( "default_account: set partition to %s", job_desc->partition );
+      break;
     }
+  list_iterator_destroy(part_iterator);
+  }
 
-    /* check job's account */
-    debug( "default_account: checking job account" );
-    if (job_desc->account){
-		debug("default_account: account %s requested", job_desc->account);
-		account = job_desc->account ;
-    } else {
-        /* else check user's default account */
-        account = _get_default_account( job_desc->user_id );
-        debug( "default_account: got users default account %s", account );
+  /* look for default partition if we set partition above */
+  if (job_desc->partition != NULL ){
+    debug( "default_account: checking for default partition" );
+    part_iterator = list_iterator_create(part_list);
+    while ((part_ptr = (struct part_record *) list_next(part_iterator))) {
+      if (part_ptr->flags & PART_FLAG_DEFAULT){
+        debug("default_account: found default partition %s", part_ptr->name);
+        xstrcat(job_desc->partition, ",");
+        xstrcat(job_desc->partition, part_ptr->name);
+        info( "default_account: set partition to %s", job_desc->partition );
+        break;
+      }
     }
+    list_iterator_destroy(part_iterator);
+  }
 
-    /* look for partition matching account name */
-	part_iterator = list_iterator_create(part_list);
-	while ((part_ptr = (struct part_record *) list_next(part_iterator))) {
-        debug( "default_account: checking partition %s against %s",
-                part_ptr->name, account );
-		if (strcmp(part_ptr->name,account) == 0){
-            job_desc->partition = xstrdup(account);
-            info( "default_account: set partition to %s", job_desc->partition );
-            break;
-		}
-	}
-	list_iterator_destroy(part_iterator);
-
-    /* look for default partition if we set partition above */
-    if (job_desc->partition != NULL ){
-        debug( "default_account: checking for default partition" );
-        part_iterator = list_iterator_create(part_list);
-        while ((part_ptr = (struct part_record *) list_next(part_iterator))) {
-            if (part_ptr->flags & PART_FLAG_DEFAULT){
-                debug( "default_account: found default partition %s",
-                        part_ptr->name );
-                xstrcat(job_desc->partition, ",");
-                xstrcat(job_desc->partition, part_ptr->name);
-                info( "default_account: set partition to %s",
-                        job_desc->partition );
-                break;
-            }
-        }
-        list_iterator_destroy(part_iterator);
-    }
-
-    if (job_desc->partition == NULL){
-        debug( "default_account: no partition set and no match found for %s",
-                account
-                );
-    }
-
-	return SLURM_SUCCESS;
+  if (job_desc->partition == NULL){
+    debug(
+        "default_account: no partition set and no match found for %s",
+        account
+        );
+  }
+  return SLURM_SUCCESS;
 }
 
 extern int job_modify(struct job_descriptor *job_desc,
